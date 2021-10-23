f1ee3f8af20654aa9f2ae3666f41bb4e6680dc93
jiro4 <joachim.guitton@gmail.com>
2021-10-23 15:18:12 +0200

    Version 2

---
+++ app/src/main/java/com/example/pts3/Ajouter_a_frigo_manuel.java
@@ -0,0 +1,14 @@
+package com.example.pts3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ajouter_a_frigo_manuel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_afrigo_manuel);
+    }
+}
--- app/src/main/java/com/example/pts3/MainActivity.java
+++ app/src/main/java/com/example/pts3/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.pts3;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button mes_conteneurs;
+    private Button ma_liste_de_cources;
+    private Button idees_recettes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+            mes_conteneurs = findViewById(R.id.id_activity_main_mes_conteneurs);
+            ma_liste_de_cources = findViewById(R.id.id_activity_main_ma_liste_de_courses);
+            idees_recettes = findViewById(R.id.id_activity_main_idee_recettes);
+
+            mes_conteneurs.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), Mes_conteneurs.class);
+                    startActivity(intent);
+
+                }
+            });
+
+            ma_liste_de_cources.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), Mes_listes.class);
+                    startActivity(intent);
+                }
+            });
+
+            idees_recettes.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), Idee_recettes.class);
+                    startActivity(intent);
+                }
+            });
+
     }
 }
